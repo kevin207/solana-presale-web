071b58fe1efcef30fa700aadb629ae38a7ec4fc1
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-06-26 13:23:59 +0700

    Update Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "@/views/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen flex-col items-center justify-start space-y-10 p-5 bg-violet-800">
+    <main className="flex w-full min-h-screen flex-col items-center justify-start p-5 bg-violet-800">
       <Navbar />
       <HeroSection />
       <About />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,19 +7,29 @@ import ConnectWallet from "./ConnectWallet";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row w-full max-w-7xl justify-between items-center">
+    <nav className="bg-violet-800 z-10 fixed top-0 py-4 flex flex-row w-full max-w-7xl justify-between px-8 lg:px-0 items-center">
       <Link href="/" className="hidden lg:flex items-center gap-1">
-        <Image src="/assets/icons/icon-full.svg" width={300} height={60} alt="logo" />
+        <Image
+          src="/assets/icons/icon-full.svg"
+          width={250}
+          height={60}
+          alt="logo"
+        />
       </Link>
       <Link href="/" className="flex: lg:hidden flex items-center gap-1">
-        <Image src="/assets/icons/icon-only.svg" width={60} height={60} alt="logo" />
+        <Image
+          src="/assets/icons/icon-only.svg"
+          width={60}
+          height={60}
+          alt="logo"
+        />
       </Link>
 
-      <ul className="hidden lg:flex flex-row gap-4">
+      <ul className="hidden lg:flex flex-row gap-8">
         {navigations.map((nav, index: number) => (
           <li key={index}>
             <Link href={nav.path}>
-              <span className="text-accent text-lg font-medium whitespace-nowrap hover:text-main duration-300">
+              <span className="text-accent text-sm font-medium whitespace-nowrap hover:text-main duration-300">
                 {nav.name}
               </span>
             </Link>
