68d721b65009e626ee83a7cbaf660db72c491aa5
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-07-23 14:12:58 +0700

    update navigation

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,12 +3,13 @@ import { sepolia, avalancheFuji, baseSepolia, bscTestnet } from "viem/chains";
 
 export const navigations = [
   { name: "About", path: "#about" },
-  { name: "Overview", path: "#faq" },
-  { name: "How it Works", path: "#contact" },
+  // { name: "Overview", path: "#faq" },
+  // { name: "How it Works", path: "#contact" },
   { name: "ICO Token", path: "#ico" },
   { name: "Roadmap", path: "#roadmap" },
   { name: "Team", path: "#team" },
   { name: "FAQ", path: "#faq" },
+  { name: "Portal Bridge", path: "https://token-bridge-dusky.vercel.app" },
 ];
 
 export const features = [
